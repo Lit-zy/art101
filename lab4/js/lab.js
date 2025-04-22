7a519a33db7d2d5132932aeef7616344ec991d4f
Lit-zy <litzymozqueda@gmail.com>
2025-04-21 17:55:20 -0700

    Final Lab 4
    This is the end result of my lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,42 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Sun Collage Art and a description on how to do it step by step
+// Author: Litzy <lamozque@ucsc.edu>
+// Date: April 21, 2025
 
 // Constants
+
+// How to create a paper collage of the sun: 
+// Gather necessary materials: construction paper for base, magazines, scissors, an Exacto knife, glue stick, Modge Podge gloss, and one foam brush. 
+// Skim through a magazine and identify and gather different shades of yellow.
+     // Use scissors to cut the paper from the magazine or rip it out with your hands.
+// Using the scissors, cut out various shapes of these yellow shades. 
+    // For a more advanced technique, use an Exacto knife instead of scissors for a cleaner cut.
+// Use the glue stick to start attaching the various shapes of magazine paper onto the construction base paper. 
+    // Make sure you are creating a circle shape with your magazine paper to mimic the shape of the sun. 
+// Continue to layer different shapes and shades of yellow magazine paper, using a layering technique.
+    // Remember to let the glued paper dry in between to prevent tearing. 
+// Once you have formed a circle shape to mimic the sun, use scissors to cut out triangle-shaped pieces of magazine paper. 
+    // You will need to cut out a minimum of eight triangle-shaped pieces of magazine paper.
+// Use the glue stick to attach the triangle-shaped pieces to the outer edges of the circle shape to mimic rays of the sun. 
+    //Remember to evenly distribute the eight pieces of magazine paper around the circle, you may need to cut more. 
+// Let all the pieces you glued on the construction paper dry for a minimum of an hour 
+// Once the pieces have dried, use the foam brush to add a layer of the Modge  Podge gloss to properly seal your collage 
+    // Make sure you evenly distribute the gloss and cover the whole paper. 
+// Set out to dry on an even surface for a minimum of an hour  
+// Finished! 
 
 // Functions
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
+    // some code here
+    // return results;
+  }
+  
+  function main() {
+    console.log("Main function started.");
+    // the code that makes everything happen
+  }
+  
+  // let's get this party started
+  main();
+  
