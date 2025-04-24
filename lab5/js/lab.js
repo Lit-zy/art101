0b81b5a91b5940bd192e4fb09f4c92ea6cafbfcf
Lit-zy <litzymozqueda@gmail.com>
2025-04-23 22:51:39 -0700

    lab.js-lab5
    This is my final JavaScript file for lab5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 5: Data Types and Variables 
+// Author: Litzy M. Franyutti <lmoque@ucsc.edu>
+// Date: April 24
 
-// Constants
+// Define Variables
 
-// Functions
+//Create a variable called carName and assign the value "Taurus" to it, then print it.
+let carName = "Camry";
+//Create a variable called carYear and assign the value to 1995 to it and display it.
+let carYear = 2014;
+//Create and assign a variable called currentYear
+let curentYear = 2025;
+//Create a variable called carColor and assign the value "Gray" to it 
+let carColor = "Gray";
+//Create a boolean variable called ownIt and assign the value "True"
+let ownIt= true;
+
+// Calculate
+
+//Assign carAge to the difference between currentYear and carYear and display it.
+let carAge = curentYear - carYear; 
+
+// output
+document.writeln("Make:" + carName + "<br>");
+document.writeln("Year:" + carYear + "<br>");
+document.writeln("Color:" + carColor + "<br>");
+document.writeln("Own-It:" + true + "<br>");
+document.writeln ("Age:" + carAge + "years<br>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -13,7 +34,7 @@ function myFunction(param1, param2) {
 }
 
 function main() {
-  console.log("Main function started.");
+  document.writeln("Main function started.");
   // the code that makes everything happen
 }
 
