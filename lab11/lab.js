d2b2222cd02a8c6804215a1f96c77670ea6a455b
Lit-zy <litzymozqueda@gmail.com>
2025-05-19 18:51:11 -0700

    Lab 11

---
+++ lab11/lab.js
@@ -0,0 +1,27 @@
+//Lab J.S. - JavaScript Events and Forms 
+//Requirements: Experimenting with JS events 
+//Author: Litzy M. Franyutti 
+//Date: May 19, 2025
+
+
+//Sorts the characters of a string in alphabetical order
+function sortString(inputString) {
+//We have to convert our string to an array and back again to sort it 
+return inputString.split('').sort().join('');
+}
+
+//click listener for the button 
+$("#submit").click(function(){
+
+//get value of input field 
+const userName = $("#user-name").val();
+
+//now lets sort it 
+userNameSorted = sortString(userName);
+
+//append a new div to our input div 
+$("#output").append('<div class="text"><p>' + userNameSorted + '</p></div>');
+});
+
+
+
