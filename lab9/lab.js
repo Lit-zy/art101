add4fd40c251fe0ff7ecd5d4705e7d7ebd0b630f
Lit-zy <litzymozqueda@gmail.com>
2025-05-29 23:22:05 -0700

    Lab 14
    Debugging Lab 9

--- lab9/lab.js
+++ lab9/lab.js
@@ -5,7 +5,7 @@
 
 
 //add button to challenge section 
-$("#Challenge").append("<button id='button-challenge'>Click Me</button>");
+$("#Challenge").append("<button id='button-challenge'>Click If You Want</button>");
 //add a click listener to the challenge button 
 $("#button-challenge").click(function(){
 //now add (or subtract) the "special" class to the section 
@@ -14,33 +14,42 @@ $("#Challenge").toggleClass("special");
 
 
 //hide the cat image when the page loads
-$("#imageWrapperID").hide();
+$("#imageOne").hide();
 //add a click listener to the challenge button for cat photo
 $("#button-challenge").click(function(){
 //toggle whether or not it is shown
-$("#imageWrapperID").toggle();
+$("#imageOne").toggle();
 });
 
 //add button to problem section 
-$("#Problems").append("<button id='button-problems'>Click Me</button>");
+$("#Problems").append("<button id='button-problems'>Click For A Surprise</button>");
 //add a click listener to the challenge button 
 $("#button-problems").click(function(){
  //now add (or subtract) the "special" class to the section 
 $("#Problems").toggleClass("special");
 });
 
-//hide the capybara image when the page loads 
-$("#imageCapybara").hide();
-//add a click listener to the challenge button for capybara photo
-$("#button-capybara").click(function(){
+//hide the capybara image when the page loads
+$("#imageTwo").hide();
+//add a click listener to the challenge button for cat photo
+$("#button-problems").click(function(){
 //toggle whether or not it is shown
-$("#imageCapybara").toggle();
+$("#imageTwo").toggle();
 });
 
+
 //add button to results section 
-$("#Results").append("<button id='button-results'>Click Me</button>");
+$("#Results").append("<button id='button-results'>Click For Fun</button>");
 //add a click listener to the challenge button 
 $("#button-results").click(function(){
     //now add (or subtract) the "special" class to the section 
     $("#Results").toggleClass("special");
+});
+
+//hide the capybara image when the page loads
+$("#imageThree").hide();
+//add a click listener to the challenge button for cat photo
+$("#button-results").click(function(){
+//toggle whether or not it is shown
+$("#imageThree").toggle();
 });
